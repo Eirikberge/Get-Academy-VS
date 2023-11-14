8e68f61cfdd4d9e2465a197c18a69a1d46e95c24
Eirikberge <eirikbaspesletten@gmail.com>
2023-11-14 11:33:34 +0100

    .

---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args) // skal den returnere string, istedenfor void?
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+14 lLssdd`` (args[0] 14, args[1]lLssdd`).
